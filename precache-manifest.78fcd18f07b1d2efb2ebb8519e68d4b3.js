b565fb81c24cca026de614c315a5533b57f14ccb
Manikandan Shanmugam <e0284006@u.nus.edu>
2018-12-20 01:45:08 +0800

    Updates

---
+++ precache-manifest.78fcd18f07b1d2efb2ebb8519e68d4b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a552dac35ee7e2168235",
+    "url": "/reactaspire/static/js/runtime~main.a552dac3.js"
+  },
+  {
+    "revision": "cb396bba84bdd5d29bce",
+    "url": "/reactaspire/static/js/main.cb396bba.chunk.js"
+  },
+  {
+    "revision": "37df706291992cdbc741",
+    "url": "/reactaspire/static/js/1.37df7062.chunk.js"
+  },
+  {
+    "revision": "cb396bba84bdd5d29bce",
+    "url": "/reactaspire/static/css/main.45e60151.chunk.css"
+  },
+  {
+    "revision": "37df706291992cdbc741",
+    "url": "/reactaspire/static/css/1.e10f27fd.chunk.css"
+  },
+  {
+    "revision": "ce33b73a83e74f5c1097f95b9a549d05",
+    "url": "/reactaspire/index.html"
+  }
+];
